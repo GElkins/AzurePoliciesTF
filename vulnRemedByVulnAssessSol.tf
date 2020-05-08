8417e97d664bb55f5b25692783c3ac694b744211
GElkins <GElkins@users.noreply.github.com>
2020-05-08 09:28:16 -0400

    First attempt

---
+++ vulnRemedByVulnAssessSol.tf
@@ -0,0 +1,62 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_policy_definition" "policy" {
+  name         = "VulRemedByVulnAssessSol"
+  policy_type  = "BuiltIn"
+  mode         = "All"
+  display_name = "Vulnerabilities should be remediated by a Vulnerability Assessment solution"
+
+  metadata = <<METADATA
+    {
+    "category": "Security Center"
+    }
+
+METADATA
+
+
+  policy_rule = <<POLICY_RULE
+    {
+    "if": {
+      
+        "field": "type",
+        "in": "[
+            "Microsoft.Compute/virtualMachines",
+          "Microsoft.ClassicCompute/virtualMachines"]"
+      }
+    },
+    "then": {
+      "effect": "[parameters('effect')]",
+      "details": {
+          "type": "Microsoft.Security/complianceResults",
+          "name": "vulnerabilityAssessment",
+          "existenceCondition": {
+            "field": "Microsoft.Security/complianceResults/resourceStatus",
+            "in": [
+              "OffByPolicy",
+              "Healthy"
+            ]
+        } 
+    }
+  }
+POLICY_RULE
+
+
+  parameters = <<PARAMETERS
+    {
+    "effect": {
+      "type": "String",
+      "metadata": {
+        "description": "Enable or disable the execution of this policy",
+        "displayName": "Effect"
+        },
+        "allowedValues" : [
+            "AuditIfNotExists",
+            "Disabled"
+        ],
+    }
+  }
+PARAMETERS
+
+}
